ad5fcaf8dbee2e7341a33a1d2b944065138446ef
Jonathan Reyes <reyesgordillojonathandaniel@gmail.com>
2026-05-29 10:21:58 -0600

    Se agrega un comentario de prueba.

--- javascript/likes.js
+++ javascript/likes.js
@@ -2,7 +2,7 @@ const likeBtn = document.getElementById("likeBtn");
 const likeCounter = document.getElementById("likeCounter");
 
 let count = 0;
-
+// Añade un evento de click 
 export function likesCounter(){
     likeBtn.addEventListener("click", () => {
         count++;
